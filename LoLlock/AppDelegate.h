e7dd6984fb0b1dbfb7e88a9889d31e79789fd452
MrBrdo <mrbrdo@gmail.com>
2012-06-12 16:38:00 +0200

    Initial Commit

---
+++ LoLlock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoLlock
+//
+//  Created by Jan Brdo on 6/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ LoLlock/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  LoLlock
+//
+//  Created by Jan Brdo on 6/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
